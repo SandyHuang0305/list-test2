3de96f8bc1f70c3ec3dee1be8bc045e423500656
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-02-07 15:30:41 +0700

    list練習系列

--- singers.py
+++ singers.py
@@ -1,3 +1,15 @@
 singers = ['IU', 'EXO', 'jessica']
-singers.append('fx')
+singers.append('fx')#增加歌手
+print(singers)
+singers.insert(2, 'girl generation')#將少女時代塞到第2格
+print(singers)
+singers.pop()#刪掉最後一位歌手
+print(singers)
+singers.pop(3)#刪掉第x位歌手
+print(singers)
+singers.remove('IU')#刪掉第一位歌手
+print(singers)
+singers.remove('EXO')
+print(singers)
+singers.clear()#刪光list
 print(singers)
